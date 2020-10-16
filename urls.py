7c1f1d47488ec4019d8a3ea795b391015ee28468
Camelia Brennan <cameliabrennan@gmail.com>
2020-10-16 13:04:44 -0400

    updated index to django template

--- urls.py
+++ urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 
 import views
 
-# In this example, we've separated out the views.py into a new file
 urlpatterns = [
     path('', views.index),
-    path('about-me', views.about_me),
-    path('github-api-example', views.github_api_example),
+    path('blog', views.blog),
+    path('purchase', views.purchase),
 ]
 
 # Boilerplate to include static files
--- views.py
+++ views.py
@@ -2,34 +2,30 @@ import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    # This is similar to ones we have done before. Instead of keeping
-    # the HTML / template in a separate directory, we just reply with
-    # the HTML embedded here.
-    return HttpResponse('''
-        <h1>Welcome to my home page!</h1>
-        <a href="/about-me">About me</a> <br />
-        <a href="/github-api-example">See my GitHub contributions</a> <br />
-    ''')
-
-
-def about_me(request):
-    # Django comes with a "shortcut" function called "render", that
-    # lets us read in HTML template files in separate directories to
-    # keep our code better organized.
+    index_html = open("content/index.html").read()
     context = {
-        'name': 'Ash Ketchum',
-        'pokemon': 'Pikachu',
+    "content": index_html,
+    "title": "Home",
     }
-    return render(request, 'about_me.html', context)
+    return render(request,"base.html", context)
 
-
-def github_api_example(request):
-    # We can also combine Django with APIs
-    response = requests.get('https://api.github.com/users/janeqhacker/repos')
-    repos = response.json()
+def blog(request):
+    blog_html = open("content/blog.html").read()
     context = {
-        'github_repos': repos,
+    "content": blog_html,
+    "title": "My Blog",
     }
-    return render(request, 'github.html', context)
+    return render(request, "base.html", context)
+
+
+def purchase(request):
+    purchase_html = open("content/purchase.html").read()
+    context = {
+    "content": purchase_html,
+    "title": "Purchase"
+    }
+    return render(request, "base.html", context)
+
 
